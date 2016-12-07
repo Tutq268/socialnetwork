e0d13121ac892032a08a4e23eb514a0651021fc9
Tú Trần <tutran00831@gmail.com>
2016-12-07 10:52:59 +0700

    Sign in ui custom classes completed

---
+++ SocialNetwork/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  SocialNetwork
+//
+//  Created by Tú Trần on 12/7/16.
+//  Copyright © 2016 tutran. All rights reserved.
+//
+
+import UIKit
+let SHADOW_GRAY:CGFloat = 120.0 / 255.0
---
+++ SocialNetwork/FancyBtn.swift
@@ -0,0 +1,21 @@
+//
+//  FancyBtn.swift
+//  SocialNetwork
+//
+//  Created by Tú Trần on 12/7/16.
+//  Copyright © 2016 tutran. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor =  UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.7).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+}
